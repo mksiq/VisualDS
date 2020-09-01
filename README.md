d76ac8dbfe696888e9ec6e1707d7679a51f24876
mksiq <maickelsiqueira@gmail.com>
2020-09-01 16:22:34 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Visual Data Structures
+
+Random codes to visualize some algorithms 
+
+Just creating random arrays and show a bar graph so far
+
+Can be visualized at: https://mksiq.github.io/VisualDS
---
+++ script.js
@@ -0,0 +1,38 @@
+var board;
+window.addEventListener("load", loadJavaScript, false);
+
+
+
+function loadJavaScript(){
+    board = document.getElementById("board");
+    var array = [1,2,3,4,5];
+    fillBoardWithBar(array, board)
+    var generateButton = document.getElementById("bGenerate");
+    generateButton.addEventListener("click", generateBars);
+
+
+}
+
+function fillBoardWithBar(someArray, board){
+    someArray.forEach((num) => {
+        var bar = document.createElement("div");
+        bar.className = "bar";
+        bar.id = `bar${num}`;
+        var numberNode = document.createTextNode(`${num}`);
+        bar.appendChild(numberNode);
+        var size = num*25;
+        if(size == 0) size = 13;
+        bar.style.height=`${size}px`;
+        board.appendChild(bar);
+    });
+}
+
+function generateBars(){
+    var randomArray = [];
+    while(board.hasChildNodes()){
+        board.removeChild(board.lastChild);
+    }
+    while(randomArray.length < 10)
+        randomArray.push(Math.floor(Math.random() * 16));
+    fillBoardWithBar(randomArray, board)
+}
