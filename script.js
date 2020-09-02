26012a9e64e252388df0ad6b8dcdcede08ab1269
mksiq <maickelsiqueira@gmail.com>
2020-09-02 14:00:08 -0400

    Added remove duplicates

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 var board;
 var linearSearchButton;
+var removeDuplicatesButton;
 window.addEventListener("load", loadJavaScript, false);
 var randomArray = [];
 
 
 function loadJavaScript(){
     board = document.getElementById("board");
-    var array = [1,2,3,4,5];
+    let array = [1,2,3,4,5];
     fillBoardWithBar(array, board)
     var generateButton = document.getElementById("bGenerate");
     generateButton.addEventListener("click", generateBars);
     linearSearchButton = document.getElementById("bLinearSearch");
     linearSearchButton.addEventListener("click", linearSearch);
+    removeDuplicatesButton = document.getElementById("bRemoveDuplicates");
+    removeDuplicatesButton.addEventListener("click", removeDuplicates);
 }
 
 function fillBoardWithBar(someArray, board){
     someArray.forEach((num) => {
-        var bar = document.createElement("div");
+        let bar = document.createElement("div");
         bar.className = "bar";
         bar.id = `bar${num}`;
-        var numberNode = document.createTextNode(`${num}`);
+        let numberNode = document.createTextNode(`${num}`);
         bar.appendChild(numberNode);
-        var size = num*25;
-        if(size == 0) size = 13;
+        let size = num*20;
+        if(size == 0) size = 10;
         bar.style.height=`${size}px`;
         board.appendChild(bar);
     });
@@ -35,15 +38,16 @@ function generateBars(){
         board.removeChild(board.lastChild);
     }
     while(randomArray.length < 10)
-        randomArray.push(Math.floor(Math.random() * 14));
+        randomArray.push(Math.floor(Math.random() * 16));
     fillBoardWithBar(randomArray, board)
     linearSearchButton.disabled = false;
+    bRemoveDuplicates.disabled = false;
     document.getElementById("inputNumber").style.display = "block";
 }
 
 function linearSearch(){
-    var numberOfIterations = 1;
-    for(var i = 0; i<randomArray.length; i++){
+    let numberOfIterations = 1;
+    for(let i = 0; i<randomArray.length; i++){
         if(randomArray[i] == document.getElementById("inputNumber").value){
             document.getElementById("foundIndex").innerHTML = `Found at ${i}. Number of iterations: ${numberOfIterations}`;
             document.getElementById(`bar${document.getElementById("inputNumber").value}`).style.backgroundColor = `rgb(78, 28, 28)`;
@@ -51,5 +55,23 @@ function linearSearch(){
         }
         numberOfIterations++;
     }
-    document.getElementById("foundIndex").innerHTML = "Not Found. Number of iterations: " + numberOfIterations;
+    
+}
+
+function removeDuplicates(){
+    
+    let removedCount = 0;
+    while(board.hasChildNodes()){
+        board.removeChild(board.lastChild);
+    }
+    for (let i = 0; i < randomArray.length; i++) {
+        for(let k = i+1; k < randomArray.length; k++){
+            if(randomArray[i] == randomArray[k]){
+                removedCount++;
+                randomArray.splice(k,1);
+            } 
+        }
+    }
+    fillBoardWithBar(randomArray, board);
+    document.getElementById("foundIndex").innerHTML = "Number of bars removed: " + removedCount;
 }
